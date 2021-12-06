57d9bb414717ebf57834dc9263db4f6c8c374f28
Preetimarwah <88872751+Preetimarwah@users.noreply.github.com>
2021-12-06 21:59:05 +1100

    commiting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,26 @@ const Footer = () => {
         <div id="Contactme">
             <div className="Footer"> 
             <form name="contact" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact" />
   <p>
-    <label>Name <input type="text" name="name" /></label>
+    <label>Your Name: <input type="text" name="name" /></label>   
   </p>
   <p>
-    <label>Email <input type="email" name="email" /></label>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
   </p>
   <p>
     <button type="submit">Send</button>
   </p>
 </form>
+
             {/* <form name="contact" netlify>
                 <h5>Your Name</h5>
                <input name="name" type="text"/>
